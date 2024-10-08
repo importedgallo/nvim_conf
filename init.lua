cdf87d3c1dcab09f02ce01599b9e86f3998d4cba
importedgallo <andre.mongard@gmail.com>
2024-10-09 00:55:32 +1000

    installed transparency plugin

--- init.lua
+++ init.lua
@@ -23,6 +23,7 @@ require('lazy').setup({
   'williamboman/mason.nvim', -- Automatically install LSPs to stdpath for neovim
   'williamboman/mason-lspconfig.nvim', -- ibid
   'folke/neodev.nvim', -- Lua language server configuration for nvim
+  'xiyaowong/transparent.nvim',
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
